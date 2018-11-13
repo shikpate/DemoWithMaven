5d587dce8fa1a0ffe9174a395da4d3201f2fdc02
shikpate <44962270+shikpate@users.noreply.github.com>
2018-11-13 14:25:12 +0530

    Update MavenDemoApplication.java

--- MavenDemo/src/main/java/com/example/MavenDemo/MavenDemoApplication.java
+++ MavenDemo/src/main/java/com/example/MavenDemo/MavenDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MavenDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MavenDemoApplication.class, args);
+		
 		System.out.println("Hello World");
 	}
 }
